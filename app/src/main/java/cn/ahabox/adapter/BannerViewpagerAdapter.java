81d5c359b2888c716f73dcb92ac1217e0b23a843
libo <libo@goobird.com>
2017-12-19 20:08:56 +0800

    ...

--- app/src/main/java/cn/ahabox/adapter/BannerViewpagerAdapter.java
+++ app/src/main/java/cn/ahabox/adapter/BannerViewpagerAdapter.java
@@ -79,5 +79,6 @@ public class BannerViewpagerAdapter extends PagerAdapter{
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View) object);
+        
     }
 }
